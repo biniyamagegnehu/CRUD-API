4859b25c9861200137f3668368f4a5fdb4750046
biniyamagegnehu <biniyamagegnehu2@gmail.com>
2025-09-28 14:52:15 +0300

    create another get api to get specific product by id

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/", (req,res) => {
     res.send("Hello")
 })
 
-app.post("/api/product",async (req,res) => {
+app.post("/api/products",async (req,res) => {
     try{
         const product = await Product.create(req.body);
         res.status(200).json(product);
@@ -18,9 +18,19 @@ app.post("/api/product",async (req,res) => {
     }
 });
 
-app.get("/api/product" ,async (req,res) => {
+app.get("/api/products" ,async (req,res) => {
     try {
         const product = await Product.find({})
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+app.get("/api/product/:id" ,async (req,res) => {
+    try {
+        const {id} = req.params; 
+        const product = await Product.findById(id)
         res.status(200).json(product)
     } catch (error) {
         res.status(500).json({message:error.message})

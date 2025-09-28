b13be450b2d5511ae87d5e4607d2852d8b1f80de
biniyamagegnehu <biniyamagegnehu2@gmail.com>
2025-09-28 13:27:49 +0300

    make a mongodb database connection

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 
 app.get("/", (req,res) => {
     res.send("Hello")
 })
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000")
+const uri = 'mongodb+srv://biniyamagegnehu2_db:%40biniyamnohaminmd@cluster0.uuk3rel.mongodb.net/crudapi?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.connect(uri).then(() => {
+        console.log("Connected to database");
+        app.listen(3000, () => {
+        console.log("server is running on port 3000")
+    })
+}).catch((err) => {
+    console.error("connection failed",err);
+    
 })
+
